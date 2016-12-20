c57d5a992a1cfe111904ff06fbdfac9965a47b0b
Felix Lange <langefx@gmail.com>
2016-12-20 11:06:42 +0100

    added a main sketch and initalize several cubes via code

---
+++ Assets/Scripts/Sketch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour {
+
+    public GameObject myPrefab;
+
+    void Start () {
+
+        for( int i = 0; i < 3; ++i)
+        {
+            float x = 3;
+            float y = 5;
+            float z = 0;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            Cube cubeScript = newCube.GetComponent<Cube>();
+            cubeScript.SetSize(1.5f);
+        }
+
+        
+	}
+	
+	void Update () {
+		
+	}
+}
